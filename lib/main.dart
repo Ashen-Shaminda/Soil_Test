7c4c37b4433e6adecd2f6fc4befc40a86b8eda81
Ashen Shaminda <lhmasdissanayake@gmail.com>
2023-04-13 11:25:39 +0530

    Added firebase libraries for database, core and initiated firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
-// ignore_for_file: prefer_const_constructors
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:soil_test/firebase/firebaseData.dart';
 import 'home/bars.dart';
 
 // import 'home_page.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  Firebase.initializeApp();
   await initialization(null);
 
   runApp(
--- lib/tabs/first_tab.dart
+++ lib/tabs/first_tab.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
@@ -9,196 +11,213 @@ class FirstTab extends StatefulWidget {
 }
 
 class _FirstTabState extends State<FirstTab> {
-  int temperature = 10;
+  late final dref = FirebaseDatabase.instance.reference().child('sensors');
+  late DatabaseReference databaseReference;
+  int temperature = 20;
+  var lights;
+  var moisture;
+  var rainDrop;
+
+  showData() {
+    dref.once().then((snapshot) {
+      String lights = snapshot.value['lights state'].toString();
+      print("Lights state : $lights");
+      String moisture = snapshot.value['moisture state'].toString();
+      print("Moisture state : $moisture");
+      String rainDrop = snapshot.value['rain drop value'].toString();
+      print("Rain Drop Value : $rainDrop");
+      // print(snapshot.value['lights state']);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    databaseReference = dref;
+  }
 
   @override
   Widget build(BuildContext context) {
+    temperature = 100;
     return Scaffold(
       backgroundColor: Colors.blueGrey[900],
-      body: SizedBox(
-        child: Scrollbar(
-          thickness: 12,
-          radius: const Radius.circular(5),
-          child: ListView(
-            children: [
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.blueGrey,
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.black54,
-                            blurRadius: 15,
-                            spreadRadius: 6,
-                            offset: Offset(3, 4),
-                          ),
-                        ],
-                      ),
-                      width: 500,
-                      height: 230,
-                      child: CircularPercentIndicator(
-                        header: const Padding(
-                          padding: EdgeInsets.only(bottom: 15.0),
-                          child: Text(
-                            'Soil Moisture Level',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.white,
-                                letterSpacing: 2),
-                          ),
-                        ),
-                        animation: true,
-                        animationDuration: 1000,
-                        radius: 150,
-                        lineWidth: 20,
-                        percent: 0.7,
-                        center: Text(
-                          '$temperature',
-                          style: const TextStyle(
-                              fontFamily: 'Kanit-Bold.ttf',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.white,
-                              letterSpacing: 2),
-                        ),
-                        progressColor: Colors.black54,
-                        backgroundColor: Colors.blueGrey.shade700,
-                        circularStrokeCap: CircularStrokeCap.butt,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.blueGrey,
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.black54,
-                            blurRadius: 15,
-                            spreadRadius: 6,
-                            offset: Offset(3, 4),
-                          ),
-                        ],
-                      ),
-                      width: 500,
-                      height: 230,
-                      child: CircularPercentIndicator(
-                        header: const Padding(
-                          padding: EdgeInsets.only(bottom: 15.0),
-                          child: Text(
-                            'Soil Temperature Level',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.white,
-                                letterSpacing: 2),
-                          ),
-                        ),
-                        animation: true,
-                        animationDuration: 1000,
-                        radius: 150,
-                        lineWidth: 20,
-                        percent: 0.7,
-                        center: Text(
-                          '$temperature %',
-                          style: const TextStyle(
-                              fontFamily: 'Kanit-Bold.ttf',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.white,
-                              letterSpacing: 2),
-                        ),
-                        progressColor: Colors.black54,
-                        backgroundColor: Colors.blueGrey.shade700,
-                        circularStrokeCap: CircularStrokeCap.butt,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.blueGrey,
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.black54,
-                            blurRadius: 15,
-                            spreadRadius: 6,
-                            offset: Offset(3, 4),
-                          ),
-                        ],
-                      ),
-                      width: 500,
-                      height: 230,
-                      child: CircularPercentIndicator(
-                        header: const Padding(
-                          padding: EdgeInsets.only(bottom: 15.0),
-                          child: Text(
-                            'Rain Drop Level',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.white,
-                                letterSpacing: 2),
-                          ),
-                        ),
-                        animation: true,
-                        animationDuration: 1000,
-                        radius: 150,
-                        lineWidth: 20,
-                        percent: 0.7,
-                        center: Text(
-                          '$temperature%',
-                          style: const TextStyle(
-                              fontFamily: 'Kanit-Bold.ttf',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.white,
-                              letterSpacing: 2),
-                        ),
-                        progressColor: Colors.black54,
-                        backgroundColor: Colors.blueGrey.shade700,
-                        circularStrokeCap: CircularStrokeCap.butt,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.blueGrey,
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.black54,
-                            blurRadius: 15,
-                            spreadRadius: 6,
-                            offset: Offset(3, 4),
-                          ),
-                        ],
-                      ),
-                      width: 500,
-                      height: 230,
-                      child: Column(
-                        children: const [
-                          Text("data"),
-                        ],
-                      ),
-                    ),
+      body: Scrollbar(
+        thickness: 12,
+        radius: const Radius.circular(5),
+        child: ListView(children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.blueGrey,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 15,
+                    spreadRadius: 6,
+                    offset: Offset(3, 4),
                   ),
                 ],
               ),
-            ],
+              width: 500,
+              height: 230,
+              child: CircularPercentIndicator(
+                header: const Padding(
+                  padding: EdgeInsets.only(bottom: 15.0),
+                  child: Text(
+                    'Soil Moisture Level',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                        letterSpacing: 2),
+                  ),
+                ),
+                animation: true,
+                animationDuration: 1000,
+                radius: 150,
+                lineWidth: 20,
+                percent: 0.7,
+                center: Text(
+                  '$lights',
+                  style: const TextStyle(
+                      fontFamily: 'Kanit-Bold.ttf',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                      letterSpacing: 2),
+                ),
+                progressColor: Colors.black54,
+                backgroundColor: Colors.blueGrey.shade700,
+                circularStrokeCap: CircularStrokeCap.butt,
+              ),
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.blueGrey,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 15,
+                    spreadRadius: 6,
+                    offset: Offset(3, 4),
+                  ),
+                ],
+              ),
+              width: 500,
+              height: 230,
+              child: CircularPercentIndicator(
+                header: const Padding(
+                  padding: EdgeInsets.only(bottom: 15.0),
+                  child: Text(
+                    'Soil Temperature Level',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                        letterSpacing: 2),
+                  ),
+                ),
+                animation: true,
+                animationDuration: 1000,
+                radius: 150,
+                lineWidth: 20,
+                percent: 0.7,
+                center: Text(
+                  '$temperature %',
+                  style: const TextStyle(
+                      fontFamily: 'Kanit-Bold.ttf',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                      letterSpacing: 2),
+                ),
+                progressColor: Colors.black54,
+                backgroundColor: Colors.blueGrey.shade700,
+                circularStrokeCap: CircularStrokeCap.butt,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.blueGrey,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 15,
+                    spreadRadius: 6,
+                    offset: Offset(3, 4),
+                  ),
+                ],
+              ),
+              width: 500,
+              height: 230,
+              child: CircularPercentIndicator(
+                header: const Padding(
+                  padding: EdgeInsets.only(bottom: 15.0),
+                  child: Text(
+                    'Rain Drop Level',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                        letterSpacing: 2),
+                  ),
+                ),
+                animation: true,
+                animationDuration: 1000,
+                radius: 150,
+                lineWidth: 20,
+                percent: 0.7,
+                center: Text(
+                  '$temperature%',
+                  style: const TextStyle(
+                      fontFamily: 'Kanit-Bold.ttf',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                      letterSpacing: 2),
+                ),
+                progressColor: Colors.black54,
+                backgroundColor: Colors.blueGrey.shade700,
+                circularStrokeCap: CircularStrokeCap.butt,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.blueGrey,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 15,
+                    spreadRadius: 6,
+                    offset: Offset(3, 4),
+                  ),
+                ],
+              ),
+              width: 500,
+              height: 230,
+              child: Column(
+                children: const [
+                  Text("data"),
+                ],
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
